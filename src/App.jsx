6bafa79813df942606e3d2c7afc46bcb701a867c
ritul-ui <ritulanand1996@gmail.com>
2025-12-31 14:00:02 +0530

    actions in v19 useTransition method

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import PostForm from "./PostForm";
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <PostForm />
+    </>
+  )
+}
+
+export default App
